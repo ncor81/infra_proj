b036336a5109a6e9240864fb392113182c61afbc
ncor81 <natnat81@gmail.com>
2026-04-20 14:53:24 -0400

    Fixed inconsistent indentation

--- infra/backend.tf
+++ infra/backend.tf
@@ -1,10 +1,10 @@
 terraform {
-    required_version = ">= 1.10"
-    backend "s3" {
-        bucket          = "iam-dev-tf-state-YOUR_ACCOUNT_ID"
-        key             = "prod/terraform.tfstate"
-        region          = "us-east-1"
-        use_lockfile    = true
-        encrypt         = true
-    }
+  required_version = ">= 1.10"
+  backend "s3" {
+    bucket       = "iam-dev-tf-state-YOUR_ACCOUNT_ID"
+    key          = "prod/terraform.tfstate"
+    region       = "us-east-1"
+    use_lockfile = true
+    encrypt      = true
+  }
 }
--- infra/variables.tf
+++ infra/variables.tf
@@ -1,16 +1,16 @@
 variable "app_name" {
-    description = "Application name used to name AWS resources"
-    type        = string
-    default     = "infra_proj"
+  description = "Application name used to name AWS resources"
+  type        = string
+  default     = "infra_proj"
 }
 
 variable "aws_region" {
-    description = "AWS region to deploy into"
-    type        = string
-    default     = "us-east-1"
+  description = "AWS region to deploy into"
+  type        = string
+  default     = "us-east-1"
 }
 
 variable "aws_account_id" {
-    description = "AWS account ID - used for ARNs and ECR image URLs"
-    type        = string
+  description = "AWS account ID - used for ARNs and ECR image URLs"
+  type        = string
 }
